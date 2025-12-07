1ba7588a36b4f74f286e9fb8deb41f25301bdfc3
Kokoa Nabatame <icecocoa.since2003@gmail.com>
2025-12-07 20:38:22 +0900

    Add user_profiles migration2

--- db/migrate/20251207000001_create_user_profiles.rb
+++ db/migrate/20251207000001_create_user_profiles.rb
@@ -1,17 +1,15 @@
 class CreateUserProfiles < ActiveRecord::Migration[7.2]
     def change
       create_table :user_profiles do |t|
-        t.references :user, null: false, foreign_key: true
+        t.references :user, null: false, foreign_key: true, index: { unique: true }
         t.string :display_name
         t.text :bio
         t.string :location
         t.string :website
-        t.boolean :is_public, default: true
-        t.boolean :allow_messages, default: true
+        t.boolean :is_public, default: true, null: false
+        t.boolean :allow_messages, default: true, null: false
   
         t.timestamps
       end
-      
-      add_index :user_profiles, :user_id, unique: true
     end
-  end
+  end  
