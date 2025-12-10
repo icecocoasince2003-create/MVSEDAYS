d65d0e210618202c9dd21290cf71b3ad5410960c
Kokoa Nabatame <icecocoa.since2003@gmail.com>
2025-12-10 20:13:23 +0900

    scss update

--- app/helpers/journals_helper.rb
+++ app/helpers/journals_helper.rb
@@ -2,10 +2,9 @@ module JournalsHelper
     def getPercent(number) 
         if number.present?
           calPercent = number/5.to_f * 100
-          # calPercent = number/10.to_f * 100
-          #↑１０段階評価の際は2行目コメントアウトを外して、1行目をコメントアウト。
+          # calPercent = number/10.to_f * 100 # 十段階評価の場合
           percent = calPercent.round
-          #↑CSSは小数が含まれると、widthの幅を指定できないので四捨五入して整数化。
+          # 四捨五入して整数化
           return percent.to_s
         else
           return 0
