2c4295beafb7e62146f4453fca1b20bb807ef84c
Kokoa Nabatame <icecocoa.since2003@gmail.com>
2025-12-17 11:55:04 +0900

    Remove setup controller after database initialization

--- config/routes.rb
+++ config/routes.rb
@@ -1,22 +1,12 @@
-﻿# Rails.application.routes.draw do
-
-  # 一時的に追加
-if Rails.application.routes.draw do
-  get 'setup/run_migrations', to: 'setup#run_migrations'
-  get 'setup/run_seeds', to: 'setup#run_seeds'
-end
-  
+﻿Rails.application.routes.draw do
   devise_for :users
 
   resources :journals do
     member do
       get :select_museum
-      # いいね機能
       post 'like', to: 'journal_likes#create'
       delete 'unlike', to: 'journal_likes#destroy'
     end
-
-    # コメント機能
     resources :journal_comments, only: [:create, :destroy]
   end
 
@@ -27,13 +17,8 @@ end
     end
   end
 
-  # マイページルート
   get 'my_page', to: 'users#my_page', as: :my_page
-
-  # ユーザー管理ルート
   resources :users, only: [:index, :show, :destroy]
-
-  # journal_tag_relations
   resources :journal_tag_relations, only: [:create, :destroy]
 
   root "journals#index"
